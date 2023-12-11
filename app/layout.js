1a464b229f6130ed38b62a4778beed29cee8b85d
dindonstudio <78362187+dindonstudio@users.noreply.github.com>
2023-12-11 10:22:19 +0100

    added fathom

--- app/layout.js
+++ app/layout.js
@@ -26,6 +26,22 @@ export default function RootLayout({ children, params }) {
             name="description"
             content="Paramour is a paris based production agency dedicated to the fashion and lifestyle industries"
           />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(f, a, t, h, o, m){
+                  a[h]=a[h]||function(){
+                      (a[h].q=a[h].q||[]).push(arguments)
+                  };
+                  o=f.createElement('script'),
+                  m=f.getElementsByTagName('script')[0];
+                  o.async=1; o.src=t; o.id='fathom-script';
+                  m.parentNode.insertBefore(o,m)
+                })(document, window, 'https://cdn.usefathom.com/tracker.js', 'fathom');
+                fathom('set', 'siteId', 'TGPHYDIK');
+              `,
+            }}
+          />
         </head>
         <AnimatePresence initial={true}>
           <body key={params.slug} >
