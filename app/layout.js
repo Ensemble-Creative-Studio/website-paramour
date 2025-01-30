cc3186e996e5e9e883dab872d089d058f6d302eb
joseph-ensemble <159160958+joseph-ensemble@users.noreply.github.com>
2025-01-30 14:56:42 +0100

    feat(analytics): change fathom to google

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,7 @@
 "use client";
 import { useEffect } from "react";
 import { AnimatePresence } from "framer-motion";
-import Head from "next/head";
 import { ReactLenis, useLenis } from "@studio-freight/react-lenis";
-import Fathom from "./fathom";
 import Script from "next/script";
 
 export default function RootLayout({ children, params }) {
@@ -21,30 +19,22 @@ export default function RootLayout({ children, params }) {
     <ReactLenis root options={{ lerp: 0.08, wheelMultiplier: 0.5 }}>
       <html lang="en">
         <head>
-        <Script src="https://cdn.usefathom.com/script.js" data-site="TGPHYDIK" defer />
-
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=AW-674321134" strategy="afterInteractive" />
+        <Script id="gtag" strategy="afterInteractive">
+            {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-674321134');
+            `}
+        </Script>
+        
           <title>Paramour</title>
           <meta
             name="description"
             content="Paramour is a paris based production agency dedicated to the fashion and lifestyle industries"
           />
 
-          {/* <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                (function(f, a, t, h, o, m){
-                  a[h]=a[h]||function(){
-                      (a[h].q=a[h].q||[]).push(arguments)
-                  };
-                  o=f.createElement('script'),
-                  m=f.getElementsByTagName('script')[0];
-                  o.async=1; o.src=t; o.id='fathom-script';
-                  m.parentNode.insertBefore(o,m)
-                })(document, window, 'https://cdn.usefathom.com/tracker.js', 'fathom');
-                fathom('set', 'siteId', 'TGPHYDIK');
-              `,
-            }}
-          /> */}
         </head>
         <AnimatePresence initial={true}>
           <body key={params.slug} >
