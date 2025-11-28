5d41b5501e180e559bc111c1b658dddc643e2c9f
PaulMazeauEnsemble <paul@ensemble.ooo>
2025-11-28 17:29:39 +0100

    fix : /studio bug

--- app/(studio)/studio/[[...index]]/page.jsx
+++ app/(studio)/studio/[[...index]]/page.jsx
@@ -1,26 +1,18 @@
 'use client';
 import "../../../bo.css";
-
-import { useLenis } from '@studio-freight/react-lenis';
-import { useEffect } from 'react'; // Import useEffect
-
+import { useEffect } from 'react';
 import { NextStudio } from 'next-sanity/studio';
 import config from '../../../../sanity.config';
 
 export default function StudioPage() {
-  const lenis = useLenis(({ scroll }) => {
-    lenis.destroy();
-  });
-
-  // Call lenis.destroy() when the component unmounts
-
-
-
+  // Lenis has been removed from the studio layout, so we don't need to destroy it anymore.
+  
   return (
     <div>
-      <div className='py-4' href="/">
-        <a  href='/'className="p-2 ml-2 border  my-4 mix-blend-difference text-white">Go to website</a>
+      <div className='py-4'>
+        <a href='/' className="p-2 ml-2 border my-4 mix-blend-difference text-white">Go to website</a>
       </div>
       <NextStudio config={config} />
     </div>
-  );}
+  );
+}
