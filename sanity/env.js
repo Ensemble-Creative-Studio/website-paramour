c4d9cee8271d55c75038db79df9b4bd94ea1fd46
PaulMazeauEnsemble <paul@ensemble.ooo>
2025-12-01 15:15:08 +0100

    add  hook secret ton .Env

--- sanity/env.js
+++ sanity/env.js
@@ -1,6 +1,11 @@
 export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-08-04'
 
+export const hookSecret = assertValue(
+    process.env.NEXT_PUBLIC_SANITY_HOOK_SECRET,
+    "Missing environment variable: NEXT_PUBLIC_SANITY_HOOK_SECRET"
+);
+
 export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
 export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
 export const useCdn = false
