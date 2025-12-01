9a99b60d975fc5dec900e3becac6bc493aa3e635
PaulMazeauEnsemble <paul@ensemble.ooo>
2025-12-01 15:16:51 +0100

    fix build error

--- sanity/env.js
+++ sanity/env.js
@@ -1,10 +1,8 @@
 export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-08-04'
 
-export const hookSecret = assertValue(
-    process.env.NEXT_PUBLIC_SANITY_HOOK_SECRET,
-    "Missing environment variable: NEXT_PUBLIC_SANITY_HOOK_SECRET"
-);
+export const hookSecret = 
+    process.env.NEXT_PUBLIC_SANITY_HOOK_SECRET || ''
 
 export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
 export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
